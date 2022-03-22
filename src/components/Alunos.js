1f4db5e3e83dc9f467ae9aebea3bffbada31b754
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-03-22 14:16:28 -0300

    Creating table with React

--- src/components/Alunos.js
+++ src/components/Alunos.js
@@ -1,9 +1,51 @@
-function Alunos(){
-    return(
-        <div>
-            <p>Esta é a pagina Alunos.</p>
-        </div>
-    )
+import React from "react";
+import {Table, table} from "react-bootstrap";
+
+
+class Alunos extends React.Component{
+
+    constructor(props){
+        super(props);
+
+
+        this.state = {
+            alunos : []
+        }
+    }
+
+    render(){
+        return(
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Opções</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Wagner</td>
+                        <td>Wagner@gmail.com</td>
+                        <td>Atualizar Excluir</td>
+                    </tr>
+                    <tr>
+                        <td>Marina</td>
+                        <td>Marina@gmail.com</td>
+                        <td>Atualizar Excluir</td>
+                    </tr>
+                    <tr>
+                        <td>Kelly</td>
+                        <td>Kelly@gmail.com</td>
+                        <td>Atualizar Excluir</td>
+                    </tr>
+                </tbody>
+
+            </Table>
+        )
+        
+        
+    }
 }
 
 export default Alunos;
